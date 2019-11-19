b0733f318d6d68ca98cf47974e6220b5ef66b408
Read <decimation001@gmail.com>
2019-11-18 19:21:10 -0600

    Initial commit

---
+++ SimpleCore/Enumerables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCore
+{
+	public static class Enumerables
+	{
+		public static bool IsNullOrEmpty<T>(IEnumerable<T> values)
+		{
+			return values == null || !values.Any();
+		}
+	}
+}
