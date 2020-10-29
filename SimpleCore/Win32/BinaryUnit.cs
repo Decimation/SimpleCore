f25470ba51dcb786f6adb6f166bdacb94f5be39f
Read <decimation001@gmail.com>
2020-10-29 01:45:21 -0500

    Misc improvements

---
+++ SimpleCore/Win32/BinaryUnit.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable IdentifierTypo
+namespace SimpleCore.Win32
+{
+	public enum BinaryUnit
+	{
+		// Bit,
+		// Byte,
+
+		Kilo = 1,
+		Mega,
+		Giga,
+		Tera,
+		Peta,
+		Exa,
+		Zetta,
+		Yotta
+	}
+}
---
+++ SimpleCore/Win32/Native.OS.cs
@@ -0,0 +1,13 @@
+namespace SimpleCore.Win32
+{
+	public static partial class Native
+	{
+		public static class OS
+		{
+			/// <summary>
+			///     Environment variable PATH
+			/// </summary>
+			public const string PATH_ENV = "PATH";
+		}
+	}
+}
