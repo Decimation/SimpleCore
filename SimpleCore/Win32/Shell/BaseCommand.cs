cc531500e22ffc968e11eb23e62c33b20b4fcbf3
Read <decimation001@gmail.com>
2020-10-31 00:58:07 -0500

    Move Win32 to NeoMemory

---
+++ SimpleCore/Win32/Shell/BaseCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SimpleCore.Win32.Shell
+{
+	/// <summary>
+	/// Represents an executable <see cref="Process"/> run with the command prompt.
+	/// </summary>
+	public abstract class BaseCommand : IDisposable
+	{
+		public Process CommandProcess { get; protected set; }
+
+		protected BaseCommand() { }
+
+		public virtual void Dispose()
+		{
+			CommandProcess.ForceKill();
+		}
+
+		public virtual void Start()
+		{
+			CommandProcess.Start();
+		}
+	}
+}
